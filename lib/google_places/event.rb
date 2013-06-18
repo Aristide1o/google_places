b2a6d1c73c7a64d204d79bc9e7fb1ce9f4d30a01
Aristide Niyungeko <niyungeko@gmail.com>
2013-06-17 20:53:36 -0700

    Fix param nanming

--- lib/google_places/event.rb
+++ lib/google_places/event.rb
@@ -1,8 +1,8 @@
 module GooglePlaces
   class Event
-    attr_accessor :review_id, :start_time, :summary, :url
-    def initialize(review_id, start_time, summary, url)
-      @review_id   = review_id
+    attr_accessor :event_id, :start_time, :summary, :url
+    def initialize(event_id, start_time, summary, url)
+      @event_id   = event_id
       @start_time  = start_time
       @summary     = summary
       @url         = url
