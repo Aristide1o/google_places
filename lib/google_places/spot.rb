607ae8011545dc2382cce6bd05ef0cf7b2611c25
Aristide Niyungeko <niyungeko@gmail.com>
2013-06-23 17:02:46 -0700

    just a safeguard for an observed bug where spot.find fails due to a nil response

--- lib/google_places/spot.rb
+++ lib/google_places/spot.rb
@@ -117,7 +117,7 @@ module GooglePlaces
         :retry_options => retry_options
       )
 
-      self.new(response['result'])
+      self.new(response['result']) if response
     end
 
     # Search for Spots with a pagetoken
@@ -258,7 +258,7 @@ module GooglePlaces
     def self.list_radar(lat, lng, api_key, sensor, options = {})
       location = Location.new(lat, lng)
       radius = options.delete(:radius) || 1000
-      types  = options.delete(:types) ||  (%w(accounting airport amusement_park aquarium art_gallery atm bakery bank bar beauty_salon bicycle_store book_store bowling_alley bus_station cafe campground car_dealer car_rental car_repair car_wash casino cemetery church city_hall clothing_store convenience_store courthouse dentist department_store doctor electrician electronics_store embassy establishment finance fire_station florist food funeral_home furniture_store gas_station general_contractor grocery_or_supermarket gym hair_care hardware_store health hindu_temple home_goods_store hospital insurance_agency jewelry_store laundry lawyer library liquor_store local_government_office locksmith lodging meal_delivery meal_takeaway mosque movie_rental movie_theater moving_company museum night_club painter park parking pet_store pharmacy physiotherapist place_of_worship plumber police post_office real_estate_agency restaurant roofing_contractor rv_park school shoe_store shopping_mall spa stadium storage store subway_station synagogue taxi_stand train_station travel_agency university veterinary_care zoo administrative_area_level_1 administrative_area_level_2 administrative_area_level_3 colloquial_area country floor geocode intersection locality natural_feature neighborhood political point_of_interest post_box postal_code postal_code_prefix postal_town premise room route street_address street_number sublocality sublocality_level_4 sublocality_level_5 sublocality_level_3 sublocality_level_2 sublocality_level_1 subpremise transit_station))
+      types  = options.delete(:types) || (%w(accounting airport amusement_park aquarium art_gallery atm bakery bank bar beauty_salon bicycle_store book_store bowling_alley bus_station cafe campground car_dealer car_rental car_repair car_wash casino cemetery church city_hall clothing_store convenience_store courthouse dentist department_store doctor electrician electronics_store embassy establishment finance fire_station florist food funeral_home furniture_store gas_station general_contractor grocery_or_supermarket gym hair_care hardware_store health hindu_temple home_goods_store hospital insurance_agency jewelry_store laundry lawyer library liquor_store local_government_office locksmith lodging meal_delivery meal_takeaway mosque movie_rental movie_theater moving_company museum night_club painter park parking pet_store pharmacy physiotherapist place_of_worship plumber police post_office real_estate_agency restaurant roofing_contractor rv_park school shoe_store shopping_mall spa stadium storage store subway_station synagogue taxi_stand train_station travel_agency university veterinary_care zoo administrative_area_level_1 administrative_area_level_2 administrative_area_level_3 colloquial_area country floor geocode intersection locality natural_feature neighborhood political point_of_interest post_box postal_code postal_code_prefix postal_town premise room route street_address street_number sublocality sublocality_level_4 sublocality_level_5 sublocality_level_3 sublocality_level_2 sublocality_level_1 subpremise transit_station))
       name  = options.delete(:name)
       keyword = options.delete(:keyword)
 
@@ -293,6 +293,7 @@ module GooglePlaces
     def self.multi_pages_request(method, multipage_request, options)
       begin
         response = Request.send(method, options)
+        
         response['results'].each do |result|
           if !multipage_request && !response["next_page_token"].nil? && result == response['results'].last
             # add next page token on the last result
